0d22f1ba66dd8651594de485e33b284af45d2120
Xiaowen Nie <xiaowennie@Xiaowens-iMac.local>
2022-04-15 13:27:55 -0700

    view questions by tags

---
+++ routes/tags.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+const { asyncHandler, csrfProtection } = require("./utils");
+const { requireAuth } = require("../auth");
+const { check, validationResult } = require("express-validator");
+
+const db = require("../db/models");
+
+router.get("/:id(\\d+)",csrfProtection, asyncHandler(async(req, res) => {
+    const tagId = parseInt(req.params.id, 10);
+    const questions = await db.Question.findAll({
+        where: {
+            tagId
+        },
+        include: [db.Tag, db.User]
+    });
+    const tag = await db.Tag.findByPk(tagId);
+    res.render('tag', { questions, tag, title: 'view questions by tags'})
+}))
+
+module.exports = router;
